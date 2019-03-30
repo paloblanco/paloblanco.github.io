8ff5391dc9fc2a7cc6c1db539bd43daa00b851e5
paloblanco <45581243+paloblanco@users.noreply.github.com>
2019-03-30 11:37:07 -0700

    Added deploy script

---
+++ ghdeploy.bat
@@ -0,0 +1,10 @@
+:: git shortcut for pushing changes. 
+call pelpub.bat
+git add -A
+git commit -m "Deploying changes"
+git push origin source
+pushd output
+git add -A
+git commit -m "Deploying changes"
+git push origin master
+popd
